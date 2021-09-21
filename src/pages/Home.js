068735e03f79a6fa52d207a419fc10c941aeb984
Yuu <anpanmamZ3624@gmail.com>
2021-09-21 11:21:58 +0900

    firebaseの導入

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default Home;
