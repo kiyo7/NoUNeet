eb8d0937eae372e6946e26fd920333bfe3f38cb6
kiyo7 <kiyo324783@icloud.com>
2021-09-25 23:30:06 +0900

    MainPage作成(途中)

---
+++ src/pages/MainPage/CompanyCard.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const CompanyCards = (props) => {
+  const { name, image, onClick } = props;
+
+  return (
+    <SCard onClick={onClick}>
+      <SImage src={image} alt="会社名" />
+      <SCompanyTitle>{name}</SCompanyTitle>
+    </SCard>
+  );
+};
+
+const SCard = styled.div`
+  width: 300px;
+  height: 300px;
+`;
+
+const SImage = styled.img`
+  border-radius: 20px;
+  border: 5px solid #131012;
+`;
+
+const SCompanyTitle = styled.h3`
+  font-size: 2rem;
+  font-family: 'Noto Sans JP', sans-serif;
+  background-image: linear-gradient(#6d5f65 0%, #544e4e 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+`;
