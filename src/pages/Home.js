cd5c3c309e59acacf6d113dd1f0df2bfb3d9ce71
miyagi-0817 <skillup0126@gmail.com>
2021-09-25 12:38:12 +0900

    トップバーの追加

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,38 @@
+import React, { useState, useContext } from "react";
+import { Redirect, Link } from "react-router-dom";
+import styled from "styled-components";
+
+// material-ui
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+// import Container from "@material-ui/core/Container";
+
 //components
-import React from 'react';
 import firebase from "../config/firebase";
+import { Topbar } from "./Topbar";
+import background from "../images/background.jpeg";
+import { AuthContext } from "../AuthService";
 
 export const Home = () => {
+  const SContainer = styled.div`
+    background-image: url(${background});
+    max-width: 100vw;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100vh;
+  `;
   return (
     <>
       <h1>Home</h1>
-      <button onClick={() => firebase.auth().signOut()}>ログアウト</button>
+      <div className="Top">
+        <Topbar />
+
+        <button onClick={() => firebase.auth().signOut()}>ログアウト</button>
+      </div>
     </>
   );
+
+  return <></>;
 };
---
+++ src/pages/Topbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export const Topbar = () => {
+  return (
+    <>
+      <div className="App">
+        <a
+          // className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          // rel="noopener noreferrer"
+        >
+          <p>MainPage</p>
+        </a>
+        <a
+          // className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          // rel="noopener noreferrer"
+        >
+          <p>Map</p>
+        </a>
+        <a
+          // className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          // rel="noopener noreferrer"
+        >
+          <p>Profile</p>
+        </a>
+        <a
+          // className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          // rel="noopener noreferrer"
+        >
+          <p>Logout</p>
+        </a>
+      </div>
+    </>
+  );
+};
